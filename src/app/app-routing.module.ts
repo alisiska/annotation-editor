a05f5bfde6518e64a19b8b51daabecf3a68d3f42
Alisa Arkalykova <49148189+alisiska@users.noreply.github.com>
2025-08-26 13:28:31 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Repositories:
+*     - repo: https://github.com/angular/angular-bazel-example release version: master-2f1abb9  asset relative path: src/app/app-routing.module.ts
+*    
+*    Copyrights:
+*     - copyright (c) 2014-2017 google, inc. http://angular.io
+*    
+*    Licenses:
+*     - MIT License
+*       SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
